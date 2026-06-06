59525027ee709757c4481c6668c03b4d5e869b4f
fm-automatiton-user <automation@fitmatch.ai>
2026-06-06 04:41:13 +0000

    Update Package.swift for v0.12.1 binary distribution

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Quadrascan",
-            url: "https://github.com/graupel/ios-quadrascan-sdk/releases/download/v0.12.0/Quadrascan-0.12.0.xcframework.zip",
-            checksum: "1c933258047f8a894d415d3ac679c9fef8be2e0f553e6d16345720257233542a"
+            url: "https://github.com/graupel/ios-quadrascan-sdk/releases/download/v0.12.1/Quadrascan-0.12.1.xcframework.zip",
+            checksum: "618098aab8c91d0b590bb098e8eba09390d36a987eeb9133f002f95e1c74dbd1"
         )
     ]
 )
